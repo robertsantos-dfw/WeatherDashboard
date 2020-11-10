28cb413f4ba026e0724d4da744095af37bfef26a
Robert Santos <robertsantos.dfw@gmail.com>
2020-11-10 07:51:00 -0600

    updated CSS and Index to have container and styling

--- script.js
+++ script.js
@@ -1,48 +1,24 @@
-var button = document.querySelector(".button")
-var inputValue = document.querySelector(".inputValue")
-var name = document.querySelector(".name")
-var desc = document.querySelector(".desc")
-var temp = document.querySelector(".temp")
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputValue");
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
 
 button.addEventListener("click", function() {
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + inputValue.value + `&units=imperial` + `&appid=218d741e070c3405fc59e124350e93ca`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .then(data => {
-            var nameValue = data["name"];
-            var tempValue = data["main"]["temp"];
-            var descValue = data["weather"][0]["description"];
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=` + inputValue.value + `&appid=fe25e99f2eabe262e23505065d0570b2`)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .then((data) => {
+            let nameValue = data["name"];
+            let tempValue = data["main"]["temp"];
+            let descValue = data["weather"][0]["description"];
 
             name.innerHTML = nameValue;
             temp.innerHTML = tempValue;
             desc.innerHTML = descValue;
-        })
+        });
+});
 
-    .catch(err => alert("Wrong City Name!"))
-})
+localStorage.setItem("name", JSON.stringify(name))
 
-
-
-// $document.ready(function() {
-//     $(".button").click(function() {
-
-//         var city = $("#city").val();
-//         if (city != "")[
-//             $.ajax({
-//                 url: `https://api.openweathermap.org/data/2.5/weather?q=` + inputValue.value + `&units=imperial` + `&appid=218d741e070c3405fc59e124350e93ca`,
-//                 type: "GET",
-//                 dataType: "jsonp",
-//                 success: function(data) {
-//                     console.log(data);
-//                 }
-//             })
-//         ]
-//         else {
-//             $("error").html("Wrong City Name!");
-//         }
-//     });
-
-
-
-// })
+// need to fetch data for 5 days, create new <div card> for each day with them data
