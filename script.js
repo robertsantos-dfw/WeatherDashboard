5c94300623acab22084c073df2468955f0f14d28
Robert Santos <robertsantos.dfw@gmail.com>
2020-11-12 08:49:54 -0600

    updated local storage for cities searched

--- script.js
+++ script.js
@@ -1,62 +1,80 @@
-var button = document.querySelector(".button");
-var inputValue = document.querySelector(".inputValue");
-var name = document.querySelector(".name");
-var desc = document.querySelector(".desc");
-var temp = document.querySelector(".temp");
-var humidity = document.querySelector(".humidity")
-var windspeed = document.querySelector(".windspeed")
-var uvIndex = document.querySelector(".uv")
+var inputValue = $(".inputValue");
+var desc = $(".desc");
+var temp = $(".temp");
+var humidity = $(".humidity");
+var windspeed = $(".windspeed");
+var uvIndex = $(".uv");
+var cities = [];
 
-button.addEventListener("click", function() {
-    $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=` + inputValue.value + `&appid=fe25e99f2eabe262e23505065d0570b2`)
-        .then(resp => {
-            console.log(resp)
-            $.ajax(`https://api.openweathermap.org/data/2.5/uvi?lat=${resp.coord.lat}&lon=${resp.coord.lon}&appid=fe25e99f2eabe262e23505065d0570b2`)
-                .then(sencondresp => {
-                    console.log(sencondresp)
 
-                    let forecast = data.weather
-                    let nameValue = weather.name
-                    let tempValue = weather.temp
-                    let uvValue = weather.uvIndex
 
-                    name.innerHTML = nameValue;
-                    temp.innerHTML = tempValue;
-                    desc.innerHTML = descValue;
-                });
+$(".btn").on("click", function(e) {
+    e.preventDefault();
+    let city = inputValue.val();
 
-            let $card = $(
-                `<div class="card" style="width: 18rem;">
-                    <div class="card card-body " style="width: 18rem;">
-                    <p> ${nameValue}</p>
-                    <p> ${tempValue}</p>
-                    <p> ${descValue}</p>
-                    <p> ${uvValue}</p>
-                    <ul id="list">
+    getWeatherInfo(city)
 
-                    </ul>
-                    <p> <a href="${(youtube1)}">Youtube Tutorial</a></p>
-                    </div>
-                    </div>`
-            )
-        });
-    localStorage.setItem("name", JSON.stringify(name))
 });
 
+
+function savedsearches() {
+    localStorage.setItem("cities", JSON.stringify(cities))
+}
+
+function loadfromlocalcities() {
+    JSON.parse(localStorage.getItem("cities"));
+}
+
+let allCities = JSON.parse(localStorage.getItem("cities"));
+console.log(allCities)
+
+function displaycities() {
+    for (let i = 0; i < allCities.length; i++) {
+        let list = $("<li>").addClass("row").css({ listStyleType: "none" });
+        list.html(allCities[i])
+        $("ul").prepend(list)
+    }
+}
+
+displaycities()
+
+
+
+function getWeatherInfo(city) {
+
+    $.ajax(
+        `https://api.openweathermap.org/data/2.5/weather?q=` +
+        city +
+        `&appid=fe25e99f2eabe262e23505065d0570b2`
+    ).then((resp) => {
+        console.log(resp);
+        let citySearched = $("<div>").addClass("row").css({ textAlign: "left" });
+        citySearched.text(`${resp.name} (${moment().format('LL')})`)
+
+        $(".name").html(citySearched)
+        temp.text(`Temp: ${resp.main.temp}`)
+        humidity.text(`Humidity: ${resp.main.humidity}`)
+        windspeed.text(`WindSpeed: "deg: "${resp.wind.deg}"speed: "${resp.wind.speed}`)
+        uv.text(`UV: ${resp.main.uv}`)
+
+        //verify if city exist in cities
+        if (!cities.includes(resp.name)) {
+            cities.push(resp.name)
+            let newcity = $("<li>").addClass("row").css({ listStyleType: "none" });
+            newcity.html(resp.name)
+            $("ul").prepend(newcity)
+        }
+
+        savedsearches()
+
+
+    });
+
+}
+
+// $.ajax(`https://api.openweathermap.org/data/2.5/uvi?lat=${resp.coord.lat}&lon=${resp.coord.lon}&appid=fe25e99f2eabe262e23505065d0570b2`)
+//     .then(sencondresp => {
+//             console.log(sencondresp)
+
+
 // need to fetch data for 5 days, create new <div card> for each day with them data
-
-//this code is before adding 2nd response for lat and lon
-// button.addEventListener("click", function() {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather/daily?q=` + inputValue.value + `&appid=fe25e99f2eabe262e23505065d0570b2`)
-//         .then((response) => response.json())
-//         .then((data) => console.log(data))
-//         .then((data) => {
-//             let nameValue = data["name"];
-//             let tempValue = data["main"]["temp"];
-//             let descValue = data["weather"][0]["description"];
-
-//             name.innerHTML = nameValue;
-//             temp.innerHTML = tempValue;
-//             desc.innerHTML = descValue;
-//         });
-// });
